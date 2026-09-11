b958b51e1f1792953d8f0c14e00913f6e5bd3219
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2026-09-11 22:43:50 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -62,6 +62,8 @@ UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
+UPDATE_PACKAGE "openwrt-daede" "kenzok8/openwrt-daede" "main"
+UPDATE_PACKAGE "luci-app-minigate" "tpxcer/luci-app-minigate" "main"
 
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
